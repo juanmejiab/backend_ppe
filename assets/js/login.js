a6d99a91aa341d9e8e7bd0f27c4572e87c828909
Juan Pablo Mejía <juan.mejiab@upb.edu.co>
2022-05-14 21:35:23 -0500

    primer commit

---
+++ assets/js/login.js
@@ -0,0 +1,36 @@
+let btn_enviar, formulario;
+let corre,clave,usuario;
+
+window.onload = function()
+{
+	btn_enviar = document.getElementById("btn_enviar");
+	correo = document.getElementById("correo");
+	clave = document.getElementById("clave");
+	formulario = document.getElementById("formulario");
+	formulario.addEventListener("submit", procesarLogin);
+}
+
+function procesarLogin()
+{
+	let txt_correo, txt_clave;
+	let str_usuario, md5_clave;
+	let error = false;
+	txt_correo = correo.value;
+	txt_clave = md5(clave.value);
+
+	str_usuario = localStorage.getItem("usuario");
+	usuario = JSON.parse(str_usuario)
+
+	if(usuario)
+	{
+		if(usuario.correo == txt_correo && usuario.clave == txt_clave)
+		{
+			cambiarSesion(true);
+			window.alert("Logeado :)");
+		}
+		else
+		{
+			window.alert("Intenta nuevamente");
+		}
+	}
+}
